2796089fca7d84e46abf3132b82898f9be72b8e9
10Dhruv10 <145593392+10Dhruv10@users.noreply.github.com>
2026-01-22 00:54:49 +0530

    Routes

---
+++ src/app/title-resolver/title-resolver.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title-resolver',
+  imports: [],
+  templateUrl: './title-resolver.component.html',
+  styleUrl: './title-resolver.component.css'
+})
+export class TitleResolverComponent {
+
+}
